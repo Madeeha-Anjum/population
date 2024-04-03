7f1f7f4d7ebc3dbbe9a9c00217d5fcc49d128b91
Madeeha-Anjum <maadeeeehaa@gmail.com>
2024-04-03 17:31:49 -0600

    added tailwind

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import React from 'react'
-
+import React from "react";
 
 const Greeting = ({ name }) => {
-    return <h1>Hello, {name}</h1>
-}
+  return <h1 className="text-red-500">Hello, {name}</h1>;
+};
 
 const App = () => {
   return (
     <div>
-        <Greeting name="world" />
+      <Greeting name="world" />
     </div>
-  )
-}
+  );
+};
 
-export default App
-
- 
-  
+export default App;
