dddea6aeb86870cab2a91f1a9fd63f7080b9ceae
Madeeha-Anjum <maadeeeehaa@gmail.com>
2024-04-03 22:55:36 -0600

    re-organized code

--- TODO.md
+++
@@ -1,4 +0,0 @@
-# TODO
-
-- [x] add tailwind
-- []  Add axios
--- src/App.js
+++ src/App.js
@@ -1,65 +1,129 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const PopulationDetails = ({ populationData }) => {
-  return (
-    <div>
-      <h1>Population Details</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Country</th>
-            <th>Population</th>
-          </tr>
-        </thead>
-        <tbody>
-          {!populationData && (
-            <tr>
-              <td colSpan="2">Loading...</td>
-            </tr>
-          )}
-
-          {populationData.populationData.map((country) => {
-            return (
-              <tr key={country.name.common}>
-                <td>{country.name.common}</td>
-                <td>{country.population}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
+const getCountriesData = async () => {
+  const response = await axios.get("https://restcountries.com/v3.1/all");
+  return response.data;
 };
 
-const Population = () => {
-  const [populationData, setPopulationData] = useState(null);
+const calcMean = (numbers) => {
+  // Mean = (Sum of Population Density Values) / (Total Number of Values)
+  let sum = 0;
+  numbers.forEach((number) => {
+    sum += number;
+  });
 
-  const getPopulationData = async () => {
-    const response = await axios.get("https://restcountries.com/v3.1/all");
+  return sum / numbers.length;
+};
 
-    return response.data;
-  };
+const calcMedian = (numbers) => {
+  // Median = Middle Value of Population Density Values
+  numbers.sort((a, b) => a - b);
+  let medianIndex = Math.floor(numbers.length / 2);
+  return numbers[medianIndex];
+};
 
-  useEffect(() => {
-    getPopulationData().then((data) => {
-      setPopulationData(data);
-    });
-  }, []);
+const calcStandardDeviation = (numbers) => {
+  // Standard Deviation = Square Root of (Sum of (Population Density - Mean)^2 / Total Number of Values)
+  let mean = calcMean(numbers);
+  let sumOfSquaredDifferences = 0;
+  numbers.forEach((number) => {
+    sumOfSquaredDifferences += Math.pow(number - mean, 2);
+  });
 
-  return (
-    <>
-      <PopulationDetails populationData={{ populationData }} />;
-    </>
-  );
+  let standardDeviation = Math.sqrt(sumOfSquaredDifferences / numbers.length);
+  return standardDeviation;
 };
 
 const App = () => {
+  const [countries, setCountries] = useState(null);
+
+  useEffect(() => {
+    getCountriesData().then((countriesData) => {
+      const countries = countriesData.map((country) => ({
+        name: country.name.common,
+        population: country.population,
+        area: country.area,
+        unMember: country.unMember,
+        currencies: country.currencies,
+        populationDensity: country.population / country.area,
+      }));
+
+      setCountries(countries);
+    });
+  }, []);
+
+  if (!countries) {
+    return <div>Loading...</div>;
+  }
+
+  const populationDensityMean = calcMean(
+    countries.map((country) => country.populationDensity)
+  );
+  const populationDensityMedian = calcMedian(
+    countries.map((country) => country.populationDensity)
+  );
+  const populationDensityStandardDeviation = calcStandardDeviation(
+    countries.map((country) => country.populationDensity)
+  );
+
+  const countUNMembers = countries.filter(
+    (country) => country.unMember === true
+  ).length;
+
+  const countriesUsingEuro = countries.filter(
+    (country) => country.currencies?.EUR
+  ).length;
+
   return (
-    <div>
-      <Population />
-    </div>
+    <>
+      <div className="container p-4 mx-auto prose">
+        <h1>Population Details</h1>
+        <table className="table-auto">
+          <tbody>
+            <tr>
+              <td>Mean</td>
+              <td>{populationDensityMean.toFixed(2)}</td>
+            </tr>
+            <tr>
+              <td>Median</td>
+              <td>{populationDensityMedian.toFixed(2)}</td>
+            </tr>
+            <tr>
+              <td>Standard Deviation</td>
+              <td>{populationDensityStandardDeviation.toFixed(2)}</td>
+            </tr>
+            <tr>
+              <td>Countries apart of the UN</td>
+              <td>{countUNMembers}</td>
+            </tr>
+            <tr>
+              <td>Countries using Euro</td>
+              <td>{countriesUsingEuro}</td>
+            </tr>
+          </tbody>
+        </table>
+        <br />
+        <table className="table-auto">
+          <thead>
+            <tr className="text-left">
+              <th>Country Name</th>
+              <th>Population</th>
+              <th>Population density</th>
+            </tr>
+          </thead>
+          <tbody>
+            {countries.map((country) => (
+              <tr key={country.name}>
+                <td>{country.name}</td>
+                <td>{country.population}</td>
+                <td>{country.populationDensity.toFixed(2)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
